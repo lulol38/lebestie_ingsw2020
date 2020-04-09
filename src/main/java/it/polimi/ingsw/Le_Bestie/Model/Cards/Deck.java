0c338ba251f46c44388ed011f8ec30b222810ef3
davidecarini <davidecarini>
2020-04-09 15:49:33 +0200

    Update deck

--- src/main/java/it/polimi/ingsw/Le_Bestie/Model/Cards/Deck.java
+++ src/main/java/it/polimi/ingsw/Le_Bestie/Model/Cards/Deck.java
@@ -11,25 +11,38 @@ import java.util.Collections;
 public class Deck {
 
     private static ArrayList<GodCard> deck;
-    private static String godCard2PlayersPath="DatabaseGodCard/Deck2Players";
-    private static String godCard3PlayersPath="DatabaseGodCard/Deck3Players";
+    private static String godCard2PlayersPath="DatabaseGodCard/Deck2Players.csv";
+    private static String godCard3PlayersPath="DatabaseGodCard/Deck3Players.csv";
 
-    public Deck(){
+    public Deck(int numplayers) throws IOException{
+        if(numplayers==2)
+        {
+            deck=loadsCardFromFile2Players(godCard2PlayersPath);
+        }
+        else
+        {
+            deck=loadsCardFromFile2Players(godCard3PlayersPath);
+
+        }
+        this.shuffleDeck();
 
     }
 
-    public void setDeck(ArrayList<GodCard> deck) {
-        this.deck = deck;
+
+    public boolean assignCard(Player p){
+        if(!deck.isEmpty())
+        {
+            GodCard card=deck.remove(deck.size()-1);
+            p.setGodCard(card);
+            return true;
+        }
+        else
+            return false;
     }
 
-    public void createDeck(ArrayList<GodCard> list){ }
-
-
-
-
-    public ArrayList<GodCard> loadsCardFromFile(String godCard2PlayersPath)throws IOException
+    public ArrayList<GodCard> loadsCardFromFile2Players(String godCard2PlayersPath)throws IOException
     {
-        ArrayList<GodCard> gc=new ArrayList<GodCard>();
+        ArrayList<GodCard> cards=new ArrayList<GodCard>();
         String currentLine = "";
         FileReader fileReader= new FileReader(godCard2PlayersPath);
         BufferedReader br = new BufferedReader(fileReader);
@@ -38,36 +51,49 @@ public class Deck {
             String[] s = currentLine.split(",");
             switch (s[0])
             {
-                case "Apollo":gc.add(new Apollo(s[0]));break;
-                case "Artemis":gc.add(new Artemis(s[0]));break;
-                case "Prometheus":gc.add(new Prometheus(s[0]));break;
-                case "Athena":gc.add(new Athena(s[0]));break;
-                case "Atlas":gc.add(new Atlas(s[0]));break;
-                case "Demeter":gc.add(new Demeter(s[0]));break;
-                case "Hephaestus":gc.add(new Hephaestus(s[0]));break;
-                case "Minotaur":gc.add(new Minotaur(s[0]));break;
-                case "Pan":gc.add(new Pan(s[0]));break;
-
+                case "Apollo":cards.add(new Apollo(s[0]));break;
+                case "Artemis":cards.add(new Artemis(s[0]));break;
+                case "Prometheus":cards.add(new Prometheus(s[0]));break;
+                case "Athena":cards.add(new Athena(s[0]));break;
+                case "Atlas":cards.add(new Atlas(s[0]));break;
+                case "Demeter":cards.add(new Demeter(s[0]));break;
+                case "Hephaestus":cards.add(new Hephaestus(s[0]));break;
+                case "Minotaur":cards.add(new Minotaur(s[0]));break;
+                case "Pan":cards.add(new Pan(s[0]));break;
             }
             c++;
         }
         br.close();
-        return gc;
+        return cards;
     }
 
-
-    public boolean assignCard(Player p){
-        if(!deck.isEmpty())
-        {
-            GodCard gc=deck.remove(deck.size()-1);
-            p.setGodCard(gc);
-            return true;
+    public ArrayList<GodCard> loadsCardFromFile3Players(String godCard3PlayersPath)throws IOException
+    {
+        ArrayList<GodCard> cards=new ArrayList<GodCard>();
+        String currentLine = "";
+        FileReader fileReader= new FileReader(godCard2PlayersPath);
+        BufferedReader br = new BufferedReader(fileReader);
+        int c=1;
+        while ((currentLine = br.readLine()) != null) {
+            String[] s = currentLine.split(",");
+            switch (s[0])
+            {
+                case "Apollo":cards.add(new Apollo(s[0]));break;
+                case "Artemis":cards.add(new Artemis(s[0]));break;
+                case "Athena":cards.add(new Athena(s[0]));break;
+                case "Atlas":cards.add(new Atlas(s[0]));break;
+                case "Demeter":cards.add(new Demeter(s[0]));break;
+                case "Minotaur":cards.add(new Minotaur(s[0]));break;
+                case "Pan":cards.add(new Pan(s[0]));break;
+            }
+            c++;
         }
-        else
-            return false;
+        br.close();
+        return cards;
     }
 
     private void shuffleDeck(){
         Collections.shuffle(deck);
     }
+
 }
