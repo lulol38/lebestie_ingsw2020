50a262639aa39f2715f36d30bb76e5bc108ef91e
lucaf <luca23.ferrari@mail.polimi.it>
2020-04-13 14:20:55 +0200

    Menu fxml

--- src/main/java/it/polimi/ingsw/Le_Bestie/App.java
+++ src/main/java/it/polimi/ingsw/Le_Bestie/App.java
@@ -1,5 +1,13 @@
 package it.polimi.ingsw.Le_Bestie;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -7,10 +15,19 @@ import java.io.IOException;
  *
  * ******SANTORINI GAME******
  */
-public class App
-{
+public class App extends Application {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/Menu.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main( String[] args ) throws IOException
-    {
+    {/*
         System.out.println("___________________________________________________________________________________________________________");
         System.out.println("*****************   *****************");
         System.out.println("*                   *               *");
@@ -25,6 +42,7 @@ public class App
         System.out.println("                *   *               *");
         System.out.println("                *   *               *");
         System.out.println("*****************   *               *");
-        System.out.println("___________________________________________________________________________________________________________");
+        System.out.println("___________________________________________________________________________________________________________");*/
+        launch(args);
     }
 }
