77077e21796121c95c123d4d7d48966d482e878a
davidecarini <davidecarini>
2020-04-17 17:14:01 +0200

    Insert Images for GUI

--- src/main/java/it/polimi/ingsw/Le_Bestie/Network/Client/MainClient.java
+++ src/main/java/it/polimi/ingsw/Le_Bestie/Network/Client/MainClient.java
@@ -1,17 +1,7 @@
 package it.polimi.ingsw.Le_Bestie.Network.Client;
 
-import it.polimi.ingsw.Le_Bestie.Network.Client.Client;
-import it.polimi.ingsw.Le_Bestie.Network.Messages.C2S.CloseConnection;
 import it.polimi.ingsw.Le_Bestie.View.Gui;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.io.IOException;
 
 /**
  * Main Client
