a3201d0e4fb523f9196b92fe433f152b640bbc5c
lucaf <luca23.ferrari@mail.polimi.it>
2020-05-09 18:10:34 +0200

    Builder tests

--- src/test/java/it/polimi/ingsw/Le_Bestie/Model/BuilderTest.java
+++ src/test/java/it/polimi/ingsw/Le_Bestie/Model/BuilderTest.java
@@ -6,6 +6,7 @@ import it.polimi.ingsw.Le_Bestie.Model.Builder.Builder;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class BuilderTest {
 
@@ -16,10 +17,41 @@ public class BuilderTest {
         Builder builder1 = new Builder(new Position(1,1));
         board.getGrid()[builder1.getPosition().getX()][builder1.getPosition().getY()].setBuilder(builder1);
         assertNotNull(board.getGrid()[builder1.getPosition().getX()][builder1.getPosition().getY()]);
-        //assertEquals(builder1.);
+        assertEquals(builder1.getPosition().getX(), 1);
+        assertEquals(builder1.getPosition().getY(), 1);
 
         Builder builder2 = new Builder(new Position(4,4));
         board.getGrid()[builder2.getPosition().getX()][builder2.getPosition().getY()].setBuilder(builder2);
         assertNotNull(board.getGrid()[builder2.getPosition().getX()][builder2.getPosition().getY()]);
+        assertEquals(builder2.getPosition().getX(), 4);
+        assertEquals(builder2.getPosition().getY(), 4);
+    }
+
+    public void NotMoving(){
+        Board board = new Board();
+        Builder builder1 = new Builder(new Position(1,1));
+        board.getGrid()[builder1.getPosition().getX()][builder1.getPosition().getY()].setBuilder(builder1);
+        Builder builder2 = new Builder(new Position(4,4));
+        board.getGrid()[builder2.getPosition().getX()][builder2.getPosition().getY()].setBuilder(builder2);
+
+        board.getGrid()[0][0].setLevel(4);
+        board.getGrid()[0][1].setLevel(4);
+        board.getGrid()[0][2].setLevel(4);
+        board.getGrid()[1][0].setLevel(4);
+        board.getGrid()[1][2].setLevel(4);
+        board.getGrid()[2][0].setLevel(4);
+        board.getGrid()[2][1].setLevel(4);
+        board.getGrid()[2][2].setLevel(4);
+
+        assertEquals(builder1.possibleMoves(board).size(), 0);
+
+        board.getGrid()[2][2].setLevel(0);
+        board.getGrid()[2][2].setBuilder(builder2);
+
+        assertEquals(builder1.possibleMoves(board).size(), 0);
+
+        board.getGrid()[2][2].setLevel(0);
+
+        assertEquals(builder1.possibleMoves(board).size(), 1);
     }
 }
